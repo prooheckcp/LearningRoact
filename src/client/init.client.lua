24b52c6bdcfda7f567c37aa2ca0341c813f1d349
Vasco Soares (Prooheckcp) <vasco.soares.2001@gmail.com>
2022-01-28 18:00:57 +0000

    Setting up roact

---
+++ src/client/init.client.lua
@@ -0,0 +1,13 @@
+--[[
+    This file is being used to learn Roact
+]]
+
+--Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+--Root folders
+local vendor = ReplicatedStorage.Vendor
+
+--Dependencies
+local Roact = require(vendor.Roact)
+
