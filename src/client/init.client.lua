f8c8760ed266a10ccfaf6d952a1f8bc0562d5e85
Vasco Soares (Prooheckcp) <vasco.soares.2001@gmail.com>
2022-01-28 18:11:36 +0000

    Making simple hello world

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -4,10 +4,26 @@
 
 --Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
 
 --Root folders
 local vendor = ReplicatedStorage.Vendor
 
+--Variables
+local player : Player = Players.LocalPlayer
+local playerGui : PlayerGui = player.PlayerGui
+
 --Dependencies
 local Roact = require(vendor.Roact)
 
+local app = Roact.createElement("ScreenGui", {}, {
+    HelloWorld = Roact.createElement("TextLabel", {
+        Size = UDim2.fromScale(0.5, 0.5),
+        Position = UDim2.fromScale(0.5, 0.5),
+        AnchorPoint = Vector2.new(0.5, 0.5),
+        TextScaled = true,
+        Text = "Hello World"
+    })
+})
+
+Roact.mount(app, playerGui)
